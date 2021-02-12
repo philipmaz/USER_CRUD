236448055607a1d52cb093e21a1665da6439d1e7
Filip <philip.mazurkiewicz@gmail.com>
2021-02-12 14:56:50 +0100

    new project

---
+++ myCRUD/src/main/java/pl/coderslab/DBUtil.java
@@ -0,0 +1,88 @@
+package pl.coderslab;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.*;
+
+public class DBUtil {
+    private static final String DB_URL="jdbc:mysql://localhost:3306/%s?useSSL=false&characterEncoding=utf8";
+    private static final String DB_USER="root";
+    private static final String DB_PASS="coderslab";
+
+    public static Connection connect(String dbName) throws SQLException{
+        final String urlWithDbName=String.format(DB_URL,dbName);
+        return DriverManager.getConnection(urlWithDbName,DB_USER,DB_PASS);
+
+    }
+
+    private static DataSource dataSource;
+    public static Connection getConnection() throws SQLException {
+        return getInstance().getConnection();   }
+    private static DataSource getInstance() {
+        if (dataSource == null) {
+            try {
+                Context initContext = new InitialContext();
+                Context envContext = (Context)initContext.lookup("java:/comp/env");
+                dataSource = (DataSource)envContext.lookup("jdbc/users");
+            } catch (NamingException e) { e.printStackTrace(); }
+        }
+        return dataSource;
+
+    }
+
+    public static void insert(Connection conn, String query, String... params) {
+        try ( PreparedStatement statement = conn.prepareStatement(query)) {
+            for (int i = 0; i < params.length; i++) {
+                statement.setString(i + 1, params[i]);
+            }
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void printData(Connection conn, String query, String... columnNames) throws SQLException {
+
+        try (PreparedStatement statement = conn.prepareStatement(query);
+             ResultSet resultSet = statement.executeQuery();) {
+            while (resultSet.next()) {
+                for (String param : columnNames) {
+                    System.out.print(resultSet.getString(param) + " | ");
+                }
+                System.out.println();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static final String DELETE_QUERY = "DELETE FROM tableName where id = ?";
+
+    public static void remove(Connection conn, String tableName, int id) {
+        try (PreparedStatement statement =
+                     conn.prepareStatement(DELETE_QUERY.replace("tableName", tableName));) {
+            statement.setInt(1, id);
+            statement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static final String UPDATE_QUERY = "UPDATE tableName SET columnName=? where id = ?";
+
+    public static void modify(Connection conn, String tableName, String columnName, int id, String newName) {
+        try (PreparedStatement statement =
+                     conn.prepareStatement(UPDATE_QUERY.replace("tableName", tableName).replace("columnName", columnName));) {
+            statement.setString(1, newName);
+            statement.setInt(2, id);
+            statement.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+}
---
+++ myCRUD/src/main/java/pl/coderslab/UserDao.java
@@ -0,0 +1,141 @@
+package pl.coderslab;
+
+import java.sql.*;
+import java.util.Arrays;
+
+public class UserDao {
+    public static final String CREATE_USER_QUERY = "INSERT INTO users(id,username,email,password) VALUES(default,?,?,?);";
+    private static final String READ_ID = "SELECT * FROM users WHERE id = ?;";
+    private static final String UPDATE_QUERY = "UPDATE users SET username=?, email=?, password=? WHERE id=?;";
+    private static final String DELETE_QUERY = "DELETE FROM users WHERE id=?;";
+    private static final String SELECT_ALL = "SELECT * FROM users;";
+    private static final String SELECT_LINE = "SELECT * FROM users WHERE id=?;";
+
+    public static User create(User user) {
+
+        try (Connection conn = DBUtil.connect("workshop2")) {
+            PreparedStatement statement =
+                    conn.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+
+            statement.setString(1, user.getUserName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, hashPassword(user.getPassword()));
+
+            statement.executeUpdate();
+            //Pobieramy wstawiony do bazy identyfikator, a następnie ustawiamy id obiektu user.
+            ResultSet resultSet = statement.getGeneratedKeys();
+
+            if (resultSet.next()) {
+                user.setId(resultSet.getInt(1));
+            }
+            return user;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+    public static void read_line(int userId, String... columnNames) {
+        try (Connection conn = DBUtil.connect("workshop2")) {
+            PreparedStatement statement = conn.prepareStatement(READ_ID);
+            statement.setInt(1, userId);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                for (String param : columnNames) {
+                    System.out.print(resultSet.getString(param) + " | ");
+                }
+                System.out.println();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static User read(int userId) {
+        try (Connection conn = DBUtil.connect("workshop2")) {
+            PreparedStatement statement = conn.prepareStatement(READ_ID);
+            statement.setInt(1, userId);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt(1));
+                user.setUserName(resultSet.getString(2));
+                user.setEmail(resultSet.getString(3));
+                user.setPassword(resultSet.getString(4));
+                return user;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+//   zmiana atrybutow usera odbywa sie w OP za pomoca setterow nastepnie tak zmieniony user zostaje przepisany do bazy danych za pomoca tej metody update
+    public static void update(User user) {
+        try (Connection conn = DBUtil.connect("workshop2")) {
+            PreparedStatement preparedStatement = conn.prepareStatement(UPDATE_QUERY);
+            preparedStatement.setString(1, user.getUserName());
+            preparedStatement.setString(2, user.getEmail());
+            preparedStatement.setString(3, hashPassword(user.getPassword()));
+            preparedStatement.setInt(4, user.getId());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void delete(int userId) {
+        try(Connection conn=DBUtil.connect("workshop2")){
+            PreparedStatement preparedStatement = conn.prepareStatement(DELETE_QUERY);
+            preparedStatement.setInt(1,userId);
+            preparedStatement.executeUpdate();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static User[] addToArray(User u, User[] users) {
+
+        User[] tmpUsers = Arrays.copyOf(users, users.length + 1); // Tworzymy kopię tablicy powiększoną o 1.
+        tmpUsers[users.length] = u; // Dodajemy obiekt na ostatniej pozycji.
+        return tmpUsers; // Zwracamy nową tablicę.
+
+    }
+
+    public static User[] findAll(){
+        try(Connection conn=DBUtil.connect("workshop2")){
+            PreparedStatement preparedStatement = conn.prepareStatement(SELECT_ALL);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            User[] users={};
+
+            while(resultSet.next()){
+                User user= new User();
+
+                user.setId(resultSet.getInt(1));
+                user.setUserName(resultSet.getString(2));
+                user.setEmail(resultSet.getString(3));
+                user.setPassword(resultSet.getString(4));
+
+                users=addToArray(user, users);
+            }
+            return users;
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+
+
+    public static String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+}
